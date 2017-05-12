232e592d798eacef19246ee9dcb98e9708fb13a3
Jake T <jaketepper@gmail.com>
2017-05-12 11:08:49 -0400

    major app revamp

--- src/main/java/com/theironyard/charlotte/PiLedTest/PiLedController.java
+++ src/main/java/com/theironyard/charlotte/PiLedTest/PiLedController.java
@@ -1,8 +1,13 @@
 package com.theironyard.charlotte.PiLedTest;
 
 import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.trigger.GpioSetStateTrigger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
 import java.util.Scanner;
 
 /**
@@ -12,10 +17,105 @@ import java.util.Scanner;
 @Controller
 public class PiLedController {
     public static final GpioController gpio = GpioFactory.getInstance();
-    public static GpioPinDigitalOutput myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04,   // PIN NUMBER
-            "My LED",           // PIN FRIENDLY NAME (optional)
-            PinState.LOW);      // PIN STARTUP STATE (optional)
-    // explicitly set a state on the pin object
+    public static Scanner inputScanner = new Scanner(System.in);
+    private GpioPinDigitalOutput whiteLED = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "White LED",
+            PinState.LOW);
+    private GpioPinDigitalOutput yellowLED = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "Yellow LED",
+            PinState.LOW);
+    private GpioPinDigitalOutput redLED = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "Red LED",
+            PinState.LOW);
+    private GpioPinDigitalOutput greenLED = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25, "Green LED",
+            PinState.LOW);
+    private boolean white, yellow, red, green = false;
 
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String home(Model model, String led) {
+        toggleLED(led);
+        return "home";
+    }
 
+    public void toggleLED(String led) {
+        if (led.equalsIgnoreCase("white")) {
+            if (white == false) {
+                whiteLED.high();
+                white = true;
+            } else {
+                whiteLED.low();
+                white = false;
+            }
+        } else if (led.equalsIgnoreCase("yellow")) {
+            if (yellow == false) {
+                yellowLED.high();
+                yellow = true;
+            } else {
+                yellowLED.low();
+                yellow = false;
+            }
+        } else if (led.equalsIgnoreCase("red")) {
+            if (red == false) {
+                redLED.high();
+                red = true;
+            } else {
+                redLED.low();
+                red = false;
+            }
+        } else if (led.equalsIgnoreCase("green")) {
+            if (green == false) {
+                greenLED.high();
+                green = true;
+            } else {
+                greenLED.low();
+                green = false;
+            }
+        } else if (led.equalsIgnoreCase("allOff")) {
+            whiteLED.low();
+            yellowLED.low();
+            redLED.low();
+            greenLED.low();
+            white = false;
+            yellow = false;
+            red = false;
+            green = false;
+        } else if (led.equalsIgnoreCase("blink")) {
+            blinkLEDs();
+        }
+    }
+
+    public void blinkLEDs() {
+        whiteLED.blink(1l);
+        yellowLED.blink(1l);
+        redLED.blink(1l);
+        greenLED.blink(1l);
+    }
+
+    public void consoleUI() {
+        while (true) {
+            System.out.println("Please enter your commands. \n For a list of commands, type \"help\"");
+            String inputCommand = inputScanner.nextLine();
+            if (inputCommand.equalsIgnoreCase("toggle") || inputCommand.equalsIgnoreCase("t")) {
+                System.out.println("Please enter LED to toggle:");
+                String powerled = inputScanner.nextLine();
+                toggleLED(powerled);
+            } else if (inputCommand.equalsIgnoreCase("status") || inputCommand.equalsIgnoreCase("s")) {
+                System.out.println("White LED:" + "\n" + whiteLED.getProperties() + "\n" + whiteLED.getPullResistance()
+                        + "\n" + whiteLED.getState() + "\n");
+                System.out.println("Yellow LED:" + "\n" + yellowLED.getProperties() + "\n" + yellowLED.getPullResistance()
+                        + "\n" + yellowLED.getState() + "\n");
+                System.out.println("Red LED:" + "\n" + redLED.getProperties() + "\n" + redLED.getPullResistance()
+                        + "\n" + redLED.getState() + "\n");
+                System.out.println("Green LED:" + "\n" + greenLED.getProperties() + "\n" + greenLED.getPullResistance()
+                        + "\n" + greenLED.getState() + "\n");
+            } else if (inputCommand.equalsIgnoreCase("blink") || inputCommand.equalsIgnoreCase("b")) {
+                blinkLEDs();
+                System.out.println("All LEDs are now blinking");
+            } else if (inputCommand.equalsIgnoreCase("all off") || inputCommand.equalsIgnoreCase("o")) {
+                toggleLED("allOff");
+                System.out.println("All LEDs are now turned off");
+            } else if (inputCommand.equalsIgnoreCase("help") || inputCommand.equalsIgnoreCase("h")) {
+                System.out.println("[toggle, status, blink, all off]");
+            } else {
+                System.err.println("Invalid Entry");
+            }
+        }
+    }
 }
--- src/main/java/com/theironyard/charlotte/PiLedTest/PiLedTestApplication.java
+++ src/main/java/com/theironyard/charlotte/PiLedTest/PiLedTestApplication.java
@@ -5,32 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Scanner;
 
-import static com.theironyard.charlotte.PiLedTest.PiLedController.myLed;
-
 @SpringBootApplication
 public class PiLedTestApplication {
+    public static PiLedController console;
 
 
 	public static void main(String[] args) {
-        Scanner inputScanner = new Scanner(System.in);
         SpringApplication.run(PiLedTestApplication.class, args);
-
-        while(true) {
-            System.out.println("Led [on/off]");
-            String onOff = inputScanner.nextLine();
-            if (onOff.equalsIgnoreCase("on")) {
-                PiLedController.myLed.high();
-                System.out.println("Turning LED On!");
-            } else if (onOff.equalsIgnoreCase("off")) {
-                PiLedController.myLed.toggle();
-                System.out.println("Turing LED Off..");
-            } else if (onOff.equalsIgnoreCase("low")){
-                PiLedController.myLed.low();
-                System.out.println("Setting LED to low");
-            } else {
-                System.err.println("Invalid Entry");
-            }
-		}
+        console.consoleUI();
 
 	}
 }
